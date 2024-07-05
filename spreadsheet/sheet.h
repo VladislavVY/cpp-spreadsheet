899ee94bfd612219e0af17fec833201ec7877106
VladislavVY <146252221+VladislavVY@users.noreply.github.com>
2024-07-05 03:27:17 +0300

    sheet.h

--- spreadsheet/sheet.h
+++ spreadsheet/sheet.h
@@ -1,1 +1,52 @@
-/* разместите здесь свой код */
+#pragma once
+
+#include "cell.h"
+#include "common.h"
+#include "formula.h"
+
+#include <functional>
+#include <unordered_map>
+
+class Hasher {
+public:
+    size_t operator()(const Position p) const {
+        return std::hash<std::string>()(p.ToString());
+    }
+};
+
+class Comparator {
+public:
+    bool operator()(const Position& lhs, const Position& rhs) const {
+        return lhs == rhs;
+    }
+};
+
+class Sheet : public SheetInterface {
+public:
+    using Table = std::unordered_map<Position, std::unique_ptr<Cell>, Hasher, Comparator>;
+
+    ~Sheet();
+
+    void SetCell(Position pos, std::string text) override;
+
+    CellInterface* GetCell(Position pos) override;
+    const CellInterface* GetCell(Position pos) const override;
+
+    void ClearCell(Position pos) override;
+
+    Size GetPrintableSize() const override;
+
+    void PrintValues(std::ostream& output) const override;
+    void PrintTexts(std::ostream& output) const override;
+
+    const Cell* GetCellPtr(Position pos) const;
+    Cell* GetCellPtr(Position pos);
+
+private:
+    template<typename ValuePrinter>
+    void Print(std::ostream& output, ValuePrinter printer) const;
+    
+    void ValidatePosition(Position pos) const;
+    
+	  Table cells_;
+};
